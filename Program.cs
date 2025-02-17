c497c0cad18a153e4cbbb78e1c16bb2b7b46ad4e
a <a@as-MacBook-Pro.lan.local>
2025-02-17 23:00:06 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+double weight = 700;
+Console.WriteLine(weight => 500); //tru
+
+char gender = 'm';
+Console.WriteLine(gender <= 'f'); //false
+
+double colorwavelength = 1.630;
+Console.WriteLine(colorwavelength > 1.621); //true
+
+int a = 5;
+int b = 7;
+
+bool condition = (b > a) && (a + b < a * b);
+Console.WriteLine(condition);
+
+Console.WriteLine(a = b + 1);
+
